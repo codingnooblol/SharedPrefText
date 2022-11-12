c094f761dc947c2bbf6d4b5c58116a0409e8e98e
Kim <xzsa18@naver.com>
2022-11-12 18:37:47 +0900

    저장된 자동 로그인 여부 확인 기능 추가

--- app/src/main/java/com/example/sharedpreftext/utils/ContextUtill.kt
+++ app/src/main/java/com/example/sharedpreftext/utils/ContextUtill.kt
@@ -15,5 +15,13 @@ class ContextUtill {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
         }
+
+        fun getAutoLogin(context: Context) :Boolean{
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            return pref.getBoolean(AUTO_LOGIN, false)
+
+        }
     }
 }
