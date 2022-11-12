4bfcf4da04805e08426bbe7fa4263f7c757ac356
Kim <xzsa18@naver.com>
2022-11-12 18:33:00 +0900

    ContextUtill 추가 -> 자동 로그인 저장 기능 -저장된 자동 로그인 여부 확인 작업 필요

---
+++ app/src/main/java/com/example/sharedpreftext/utils/ContextUtill.kt
@@ -0,0 +1,19 @@
+package com.example.sharedpreftext.utils
+
+import android.content.Context
+
+class ContextUtill {
+
+    companion object{
+
+        private val prefName = "TestPref"
+
+        private val AUTO_LOGIN = "AUTO_LOGIN"
+
+        fun setAutoLogin(context: Context, autoLogin: Boolean) {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+        }
+    }
+}
