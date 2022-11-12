f76318e6ff500d1cc61fdd207604a00499f77dad
Kim <xzsa18@naver.com>
2022-11-12 18:44:02 +0900

    실제로 저장 / 저장된값 조회 기능 활용

--- app/src/main/java/com/example/sharedpreftext/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreftext/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.sharedpreftext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sharedpreftext.utils.ContextUtill
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        autoLoginCheckBox.setOnCheckedChangeListener { buttonview, ischecked ->
+
+//            지금 체크된 상황을 그대로 => ContextUtill을 이용해 자동 로그인 여부로 저장
+
+            ContextUtill.setAutoLogin(this,ischecked)
+
+
+        }
+//        화면이 만들어지면 => 저장된 자동 로그인 여부 값을 => 체크박스에 반영
+        autoLoginCheckBox.isChecked = ContextUtill.getAutoLogin(this)
     }
 }
